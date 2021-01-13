156f17c821a9af8b453e346b086fe19ebde15419
Paulo854 <73476982+Paulo854@users.noreply.github.com>
2021-01-13 15:47:46 -0300

    dsadsadas

--- cadastro/usuarios.php
+++ cadastro/usuarios.php
@@ -10200,7 +10200,7 @@ print {
                       if (mysqli_num_rows($result) > 0) {
                           // output data of each row
                         while($row = mysqli_fetch_assoc($result)) {
-                        echo $row["id"]. " - Nome: <b>" . $row["usuario"]. "</b> " . $row["email"]. "<b>" . $row["bloqueado"]. "</b>";
+                        echo $row["id"]. " - Nome: <b>" . $row["usuario"]. "</b> " . $row["email"]. "<b>" . $row["bloqueado"]. "</b><br>";
                           }
                       } else {
                           echo "0 results";
