86db4e4a690d7f0408f5853a3ef6a7ceee3a823a
Paulo854 <73476982+Paulo854@users.noreply.github.com>
2021-01-13 14:21:04 -0300

    Update usuarios.php

--- cadastro/usuarios.php
+++ cadastro/usuarios.php
@@ -34,13 +34,17 @@ include('conexao/verifica_login.php');
                           die("Falha de conexão " . mysqli_connect_error());
                       }
 
-                      $sql = "SELECT id, usuario, email FROM usuarios";
+                      $sql = "SELECT id, 
+                                     usuario, 
+                                     email,
+                                     bloqueado
+                              FROM usuarios";
                       $result = mysqli_query($conexao, $sql);
 
                       if (mysqli_num_rows($result) > 0) {
                           // output data of each row
-                          while($row = mysqli_fetch_assoc($result)) {
-                              echo $row["id"]. " - Nome: <b>" . $row["usuario"]. "</b> " . $row["email"]. "<br>";
+                        while($row = mysqli_fetch_assoc($result)) {
+                        echo $row["id"]. " - Nome: <b>" . $row["usuario"]. "</b> " . $row["email"]. "<br>" . $row["bloqueado"]. "<br>";
                           }
                       } else {
                           echo "0 results";
